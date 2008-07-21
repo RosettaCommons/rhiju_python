4c95f686e9120a61e3b765d712a4085a09e24b65
Rhiju Das <rhiju@stanford.edu>
2008-07-21 01:21:01 +0000

    update to my python scripts.
    git-svn-id: https://svn.rosettacommons.org/source/workspaces/rhiju/python@23966 6189c7af-7406-0410-a2ce-f22f51069c23

--- pdb2casp.py
+++ pdb2casp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from sys  import argv,exit
-
+from os import system
 
 def Help():
     print
@@ -33,6 +33,12 @@ if argv.count('-sg'):
     pos = argv.index('-sg')
     del( argv[pos] )
 
+refinement = 0
+if argv.count('-refinement'):
+    refinement = 1
+    pos = argv.index('-refinement')
+    del( argv[pos] )
+
 infiles = argv[1:]
 
 ## if len(infiles)>5:
@@ -53,19 +59,32 @@ targetletter = 'T'
 if sg:
     targetletter = 'S'
 
+targetletter2 = '0'
+refined_tag = ''
+if refinement:
+    targetletter2 = 'R'
+    refined_tag = 'REFINED'
+
 count = 0
 for infile in infiles:
     count= count+1
-    lines = open(infile,'r').readlines()
 
-    outfile = infile[:-4] + '.casp'
+    system( 'cp '+infile+' temp.pdb')
+    system( 'replace_chain_inplace.py temp.pdb _ ' )
+    system( 'renumber_pdb_in_place.py temp.pdb' )
+
+    lines = open('temp.pdb','r').readlines()
+
+    #outfile = infile[:-4] + '.casp'
+    outfile = '%s%s%3d_%d.casp' % (targetletter,targetletter2,targetnum,count)
     fid = open(outfile,'w')
 
     fid.write('PFRMAT TS\n')
-    fid.write('TARGET %s%04d' % (targetletter,targetnum) )
+    fid.write('TARGET %s%s%3d' % (targetletter,targetletter2,targetnum) )
     if oligomer: fid.write(' OLIGOMER\n')
     fid.write('\n')
-    fid.write('AUTHOR 5377-6500-7869\n')
+    #    fid.write('AUTHOR 5377-6500-7869\n')
+    fid.write('AUTHOR  3598-2403-2731\n')
     fid.write('METHOD ROSETTA provides both ab initio and\n')
 
     fid.write('METHOD comparative models of protein domains. It\n')
@@ -79,7 +98,7 @@ for infile in infiles:
     fid.write('METHOD For some submissions, models have been refined.\n')
     fid.write('METHOD and scored with a full-atom energy function\n')
     fid.write('METHOD [Bradley et al. Science 2005;309:1868-1871].\n')
-    fid.write('MODEL  %d\n' % count)
+    fid.write('MODEL  %d %s\n' % (count,refined_tag) )
     fid.write('PARENT N/A\n')
 #    fid.write('PARENT 2f6s_A 2g03_A\n')
     for line in lines:
@@ -90,3 +109,5 @@ for infile in infiles:
     fid.write('END\n')
 
     fid.close()
+
+    system( 'rm temp.pdb' )
