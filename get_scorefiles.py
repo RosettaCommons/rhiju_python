f578ed7ba1f14a8bc76a5343d8e9d4405e323955
Rhiju Das <rhiju@stanford.edu>
2009-11-16 18:19:37 +0000

    personal python scripts.
    git-svn-id: https://svn.rosettacommons.org/source/workspaces/rhiju/python@33945 6189c7af-7406-0410-a2ce-f22f51069c23

---
+++ get_scorefiles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from os import system
+from sys import argv
+
+
+for file in argv[1:]:
+
+    assert( file[-4:] == '.out' )
+
+    scorefile = file.replace('.out','.sc')
+    if not exists( scorefile ):
+        command = 'grep SCORE '+file+' > '+scorefile
+        print( command )
+        system( command )
+
