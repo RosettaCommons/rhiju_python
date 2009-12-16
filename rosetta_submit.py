b1d32bdfbda54006d9193239f3b3613cdeb54910
Rhiju Das <rhiju@stanford.edu>
2009-12-16 21:35:22 +0000

    git-svn-id: https://svn.rosettacommons.org/source/workspaces/rhiju/python@34436 6189c7af-7406-0410-a2ce-f22f51069c23

--- rosetta_submit.py
+++ rosetta_submit.py
@@ -2,7 +2,7 @@
 
 from sys import argv,exit
 from os import system
-from os.path import basename,dirname
+from os.path import basename,dirname,expanduser
 import string
 
 if len( argv ) < 4:
@@ -20,9 +20,17 @@ except:
 lines = open(infile).readlines()
 
 bsub_file = 'bsubMINI'
+condor_file = 'condorMINI'
 fid = open( bsub_file,'w')
+fid_condor = open( condor_file,'w')
 
 tot_jobs = 0
+
+universe = 'vanilla';
+fid_condor.write('+TGProject = TG-MCB090153\n')
+fid_condor.write('universe = %s\n' % universe)
+
+HOMEDIR = expanduser('~')
 
 for line in  lines:
 
@@ -30,31 +38,44 @@ for line in  lines:
     if line[0] == '#': continue
     if string.split( line[0]) == []: continue
 
+    dir = outdir + '/$(Process)/'
+    command_line = line[:-1].replace( '-out:file:silent ', '-out:file:silent '+dir)
+    command_line = command_line.replace( '-out::file::silent ', '-out::file::silent '+dir)
+    command_line = command_line.replace( 'macosgcc', 'linuxgcc')
+    command_line = command_line.replace( 'Users', 'home')
+
+    command_line = command_line.replace( '/home/rhiju',HOMEDIR)
+
+    cols = string.split( command_line )
+    if '-total_jobs' in cols:
+        pos = cols.index( '-total_jobs' )
+        cols[ pos+1 ] = '%d' % n_jobs
+        command_line = string.join( cols )
+    if '-job_number' in cols:
+        pos = cols.index( '-job_number' )
+        cols[ pos+1 ] = '$(Process)'
+        command_line = string.join( cols )
+
     for i in range( n_jobs ):
-        dir = outdir+'/%03d/' % (tot_jobs)
-        system( 'mkdir -p '+ dirname(dir) )
-        command_line = line[:-1].replace( '-out:file:silent ', '-out:file:silent '+dir)
-        command_line = command_line.replace( '-out::file::silent ', '-out::file::silent '+dir)
-        command_line = command_line.replace( 'macosgcc', 'linuxgcc')
-        command_line = command_line.replace( 'Users', 'home')
+        dir_actual = dir.replace( '$(Process)', '%d' % i)
+        system( 'mkdir -p '+ dirname(dir_actual) )
 
-        cols = string.split( command_line )
-        if '-total_jobs' in cols:
-            pos = cols.index( '-total_jobs' )
-            cols[ pos+1 ] = '%d' % n_jobs
-            command_line = string.join( cols )
-        if '-job_number' in cols:
-            pos = cols.index( '-job_number' )
-            cols[ pos+1 ] = '%d' % i
-            command_line = string.join( cols )
-
-
-        command =  'bsub -W 16:0 '+command_line
+        command =  'bsub -W 16:0 ' + command_line.replace( '$(Process)', '%d' % i )
         fid.write( command + '\n')
 
         tot_jobs += 1
 
+    EXE = cols[ 0 ]
+    arguments = string.join( cols[ 1: ] )
+    fid_condor.write('\nexecutable = %s\n' % EXE )
+    fid_condor.write('arguments = %s\n' % arguments)
+    fid_condor.write('Queue %d' % n_jobs )
+
 fid.close()
+fid_condor.close()
 
 print 'Created bsub submission file ',bsub_file,' with ',tot_jobs, ' jobs queued. To run, type: '
 print '>source',bsub_file
+print
+print 'Created condor submission file ',condor_file,' with ',tot_jobs, ' jobs queued. To run, type: '
+print '>condor_submit',condor_file
