211e336782bc4640355c0de19f4a4c7b72488061
Rhiju Das <rhiju@stanford.edu>
2011-07-12 15:33:25 +0000

    updating rnastructure bootstrap script to include SS constraints
    git-svn-id: https://svn.rosettacommons.org/source/workspaces/rhiju/python@43159 6189c7af-7406-0410-a2ce-f22f51069c23

--- setup_rna_structure_bootstrap.py
+++ setup_rna_structure_bootstrap.py
@@ -5,11 +5,13 @@ from os import system
 from os.path import exists,dirname,basename,expanduser
 from random import randint
 from parse_options import parse_options
+import string
 
-EXE = '/home/rhiju/projects/rdat/external/RNAstructure_Rochester/RNAstructure/exe/Fold'
-
-assert( exists(EXE ) )
 HOMEDIR = expanduser('~rhiju')
+
+EXE = HOMEDIR+'/projects/rdat/external/RNAstructure_Rochester/RNAstructure/exe/Fold'
+assert( exists(EXE ) )
+
 PYDIR = HOMEDIR+'/python'
 assert( exists( PYDIR ) )
 
@@ -19,10 +21,18 @@ min_res = parse_options( argv, 'min_res', -1 )
 max_res = parse_options( argv, 'max_res', -1 )
 maxdistance = parse_options( argv, 'maxdistance', -1 )
 shape_file = parse_options( argv, 'shape_file', '' )
+force_unpair_file = parse_options( argv, 'force_unpair_file', '' )
 nboot = parse_options( argv, 'nboot', 0 )
 jobfile = parse_options( argv, 'jobfile', 'bsubRNAstructure')
 
 assert( len( argv ) == 1 )
+
+force_unpair = []
+if len( force_unpair_file ) > 0:
+    lines = open( force_unpair_file ).readlines()
+    for line in lines:
+        for m in string.split( line ):
+            force_unpair.append( int(m) )
 
 if (min_res != 1 or max_res != 1 ):
     system( '%s/slice_seq.py %s %d %d' % (PYDIR,seq_file,min_res,max_res) )
@@ -31,6 +41,12 @@ if (min_res != 1 or max_res != 1 ):
         system( '%s/slice_shape.py %s %d %d' % (PYDIR,shape_file,min_res,max_res) )
         shape_file = 'region_%d_%d_%s' % (min_res,max_res,shape_file)
 
+    if len( force_unpair) > 0:
+        force_unpair_original = force_unpair
+        force_unpair = []
+        for m in force_unpair_original:
+            if ( m >= min_res and m <= max_res ):
+                force_unpair.append( m - min_res + 1)
 
 if ( maxdistance > 0 ): EXE += ' -md %d ' % maxdistance
 
@@ -39,19 +55,42 @@ if nboot > 0: assert( len( shape_file ) > 0 )
 
 if not exists( outdir ): system( 'mkdir -p '+outdir )
 
-def outputcommand( seq_file, ct_file, shape_file, fid ):
+def outputcommand( seq_file, ct_file, shape_file, force_unpair_constraints_file, fid ):
     shape_tag = ''
     if len( shape_file ) > 0: shape_tag = ' -sh %s' % shape_file
     command_line = '%s %s %s %s' % ( EXE, seq_file, ct_file, shape_tag )
+
+    if len( force_unpair_constraints_file ) > 0:
+        command_line += ' -c %s ' % force_unpair_constraints_file
+
     outfile = '/dev/null'
     errfile = '/dev/null'
     bsub_command =  'bsub -W 16:0 -o %s -e %s %s \n' % (outfile, errfile, command_line )
     fid.write( bsub_command )
 
+force_unpair_constraints_file = ''
+if len( force_unpair ) > 0:
+    force_unpair_constraints_file = 'region_%d_%d_%s' % (min_res, max_res, 'force_unpair.con' )
+    fid = open( force_unpair_constraints_file, 'w' )
+    fid.write( 'DS:\n')
+    fid.write( '-1\n')
+    fid.write( 'SS:\n')
+    for m in force_unpair: fid.write( '%d\n' % m)
+    fid.write( '-1\n')
+    fid.write( 'Mod:\n')
+    fid.write( '-1\n')
+    fid.write( 'Pairs:\n')
+    fid.write( '-1 -1\n')
+    fid.write( 'FMN:\n')
+    fid.write( '-1\n')
+    fid.write( 'Forbids:\n')
+    fid.write( '-1 -1\n')
+
+
 fid = open( jobfile, 'w' )
 ct_file = outdir+'/'+seq_file+'.ct'
 if not exists( ct_file ):
-    outputcommand( seq_file, ct_file, shape_file, fid )
+    outputcommand( seq_file, ct_file, shape_file, force_unpair_constraints_file, fid )
 else:
     print 'Job done already ==> ', ct_file
 
@@ -77,7 +116,7 @@ if len( shape_file ) > 0 and nboot > 0:
                 fid2.write( lines[ rand_index[m] ] )
             fid2.close()
 
-            outputcommand( seq_file, boot_ct_file, boot_shape_file, fid )
+            outputcommand( seq_file, boot_ct_file, boot_shape_file, force_unpair_constraints_file, fid )
             nboot_actual += 1
 
     print 'Set up %d bootstraps for %s' % (nboot_actual,seq_file)
