9f173cdf41c3777ff3bd08b722dcfe4a46d88517
Rhiju Das <rhiju@stanford.edu>
2011-11-22 01:37:18 +0000

    easy condor testing script
    git-svn-id: https://svn.rosettacommons.org/source/workspaces/rhiju/python@46047 6189c7af-7406-0410-a2ce-f22f51069c23

---
+++ test_condor_submit.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+from sys import argv
+from os import system
+from parse_options import parse_options
+from os.path import exists, basename
+import string
+
+
+executable_dir = parse_options( argv, 'exe_dir', '' )
+database = parse_options( argv, 'd', '' )
+
+submit_script = argv[1]
+lines = open( submit_script ).readlines()
+
+executable = ''
+arguments = ''
+for line in lines:
+    if line[:10] == 'executable' and len( executable ) == 0:
+        executable = string.split( line, '=' )[1]
+        executable = executable.replace( ' ','')[:-1]
+    if line[:9] == 'arguments':
+        arguments = string.split( line, '=' )[1]
+        arguments = arguments.replace( '$Process', '0' )
+
+assert( len( executable ) > 0)
+assert( len( arguments ) > 0 )
+
+cols = arguments.split()
+pos = cols.index( '-database' )
+database_original = cols[ pos + 1 ]
+if len( database ) == 0: database = database_original
+
+if len( executable_dir ) > 0:
+    executable = executable_dir + '/'+ basename( executable )
+
+if not exists( executable ):
+    executable = executable.replace( 'linux','macos' ).replace( '/home','/Users')
+if not exists( database ):
+    database = database.replace( '/home', '/Users' )
+
+cols[ pos+1 ] = database
+arguments = string.join( cols )
+
+print executable
+assert( exists( executable ) )
+#print database
+assert( exists( database ) )
+
+command = executable +' '+arguments
+print command
+system( command )
